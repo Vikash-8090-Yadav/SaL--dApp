c11d505ed61d8bf83cc1e07b13f31aed1d631b07
Vikash <yadav621gzp@gmail.com>
2023-01-29 04:22:11 +0530

    added the id and connected to the navabr

--- Component/about.js
+++ Component/about.js
@@ -8,7 +8,7 @@ import confused from "./images/confused.jpg"
 export default function About() {
     return (
       <>
-      <div id="About">
+      <div>
 <div class="w-full my-20 z-50 sticky  rounded-3xl px-6 bg-zinc-600 bg-gradient-to-r from-indigo-800">
 
 <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
