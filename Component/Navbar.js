80c0b61f1affaf7fe9d517cefde8477e60bcb257
Vikash <yadav621gzp@gmail.com>
2023-01-29 04:21:33 +0530

    linked to the component

--- Component/Navbar.js
+++ Component/Navbar.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import {Link} from 'react-scroll';
 import {FaBars, FaTimes} from 'react-icons/fa'
-// import { useWeb3Modal, Web3Button, Web3NetworkSwitch } from '@web3modal/react'
+import { useWeb3Modal, Web3Button, Web3NetworkSwitch } from '@web3modal/react'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -20,15 +20,15 @@ const Navbar = () => {
     const links = [
         {
             id: 1,
-            link: "about",
+            link: "Home",
         },
         {
             id: 2,
-            link: "skills",
+            link: "About",
         },
         {
             id: 3,
-            link: "projects",
+            link: "Contact",
         },
         {
             id: 4,
@@ -37,18 +37,19 @@ const Navbar = () => {
     ]
     return (
         <>
-            <div className = {navBar ? 'navbar active' : 'navbar'}>
-                <h1 className = "text-3xl font-signature ml-2 text-white font-semibold max-sm:text-xl max-sm:ml-0 max-sm:mr-3">Vikash Kumar Yadav</h1>
+            <div  className = {navBar ? 'navbar active' : 'navbar'}>
+                <h1 className = "text-3xl text-extrabold text-bold text-3xl font-serif ml-2 text-white font-semibold max-sm:text-xl max-sm:ml-0 max-sm:mr-3">Sal-dAPP.eth</h1>
 
                 <ul className = "hidden md:flex text-white">
                     {links.map(({id, link}) => (
                         <li key = {id} className = "px-4 cursor-pointer font-medium md:hover:scale-125 duration-300 capitalize">
-                            <Link to={link} smooth duration={500}>{link}</Link>
+
+                            <Link to={link} smooth duration={50}>{link}</Link>
                         </li>
                     ))}
                 </ul>
                 
-                {/* <Web3Button balance="show" icon="hide" label="Connect Wallet" />  */}
+                <Web3Button balance="show" icon="hide" label="Connect Wallet" /> 
 
                 <div onClick={() => setNav(!nav)} className = "cursor-pointer pr-4 z-10 text-white md:hidden">
                     {
@@ -57,10 +58,10 @@ const Navbar = () => {
                 </div>
 
                 {nav && (
-                    <ul className = "flex flex-col justify-center items-center text-white absolute top-0 left-0 w-full h-screen bg-gradient-to-br from-[#690a4a] via-[#100e2d] to-[#08624b] max-sm:px-1">
+                    <ul className = " flex flex-col justify-center items-center text-white absolute top-0 left-0 w-full h-screen bg-gradient-to-br from-[#690a4a] via-[#100e2d] to-[#08624b] max-sm:px-1">
                     {links.map(({id, link}) => (
                         <li key = {id} className = "px-4 cursor-pointer py-6 text-2xl capitalize">
-                            <Link onClick = {() => setNav(!nav)} to={link} smooth duration={500}>{link}</Link>
+                            <Link onClick = {() => setNav(!nav)} to={link} smooth duration={5000}>{link}</Link>
                         </li>
                     ))}
                     </ul>
