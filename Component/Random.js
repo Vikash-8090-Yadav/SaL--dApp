3ef24e6d18385cf9965c60d576de0141a3b02020
Vikash KUmar Yadav <yadav621gzp@gmail.com>
2023-03-14 18:38:45 +0530

    deleted

--- Component/Random.js
+++
@@ -1,6 +0,0 @@
-export default function  Random() {
-  return(
-   <div className=' py-8 sticky big bg-gradient-to-r from-indigo-800'>  
-   </div>
-  )
-}
--- Component/logout.js
+++
@@ -1,9 +0,0 @@
-import LogoutIcon from '@mui/icons-material/Logout';
-import Link from "next/link"
-export default function   Logout() {
-  return (
-    <>
-    <LogoutIcon/>
-    </>
-  )
-}
