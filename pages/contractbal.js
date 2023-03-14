9713d6972b9a85eda38a8de455f6bdf0a00f350d
Vikash KUmar Yadav <yadav621gzp@gmail.com>
2023-03-14 18:15:37 +0530

    updated the balacne

--- pages/contractbal.js
+++ pages/contractbal.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-import {ethers} from "ethers"
-import Wallet from "../Component/wallet"
-const RPC = "https://polygon-mumbai.infura.io/v3/95688893704a4d5bac083296c3547383"
 
+import Wallet from "../Component/wallet"
 export default  function ContractBal() {
    return (
       <>
@@ -13,20 +10,3 @@ export default  function ContractBal() {
       </>
     )
   }
-=======
-import {ethers} from "ethers"
-import Wallet from "../Component/wallet"
-const RPC = "https://polygon-mumbai.infura.io/v3/95688893704a4d5bac083296c3547383"
-
-export default  function ContractBal() {
-   return (
-      <>
-      <Wallet/>
-      <h1>
-      This is the Contract Balance page
-      </h1>
-      </>
-    )
-  }
->>>>>>> 8fd5f7d792b3c968a561617eed41a3d1a4ed4e21
-  
