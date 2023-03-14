8e00fb364833c6d6bc2c32e561cb0a75a14b0ef0
Vikash KUmar Yadav <yadav621gzp@gmail.com>
2023-03-14 18:07:05 +0530

    added the final setup

--- Component/Layout.js
+++ Component/Layout.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Carousel from "./Carousel";
 import SideNavbar from "./SideNavbar";
 import AboutApp from "./AboutApp";
@@ -34,56 +33,3 @@ export default function Layout({children}) {
     </>
   )
 }
-=======
-import Navbar from "./Navbar"
-// import NavL from "./navL"
-// import NavM from "./navM"
-// import NavR from "./navR"
-import Lefts from "./lft"
-import SideNavbar from "./SideNavbar"
-import Mainc from "./mnc"
-import Cat from "./Cool"
-import AboutApp from "./AboutApp"
-import About from "./about"
-import Contact from "./contact"
-import Footer from "./footer"
-import Aboutus from "./testinomial"
-import { ToastContainer } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css";
-// import ChatwootWidget from './ChatwootWidget'
-import GoToTop from "./GoToTop";
-import Random from "./Random"
-import ParticleBack from "./Particle"
-export default function Layout({children}) {
-
-  console.log(children);
-  return (
-    <>
-      <GoToTop/>
-      {/*<Navbar/>*/}  {/* comment if u r going with the login page as this component already passed in the pages/_app.js*/}
-     
-      {<Random/>} 
-       <div className="part ">
-       <ParticleBack  />
-       </div>
-
-      <lms className = "h-screen flex flex-row justify-start"> {/* This lsm stand for the LEFT MAIN  and RIGHT-SIDE*/}
-        {/* <Lefts/> */}
-        <SideNavbar />
-        <div className = "flex-1 p-4 text-white bg-gray-300">
-          <Mainc>{children}</Mainc>
-        </div>
-        {/* <Rights/> */}
-      </lms>
-    
-      <main className = "mn">
-        <About/>
-        <AboutApp />
-        {/* <Aboutus /> */}
-        <Contact/>
-      </main>
-      <Footer/>
-    </>
-  )
-}
->>>>>>> 8fd5f7d792b3c968a561617eed41a3d1a4ed4e21
--- Component/LogoutButton.js
+++ Component/LogoutButton.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import LogoutIcon from '@mui/icons-material/Logout';
 import Link from "next/link"
 
@@ -14,19 +13,3 @@ const LogoutButton = ({ handleLogout }) => {
     )
 }
 export default LogoutButton
-=======
-import LogoutIcon from '@mui/icons-material/Logout';
-import Link from "next/link"
-
-
-const LogoutButton = ({ handleLogout }) => {
-    return (
-      <div>
-        <button className="font-bold" onClick={handleLogout}>
-            LOGOUT <LogoutIcon/>
-        </button>
-        </div>
-    )
-}
-export default LogoutButton
->>>>>>> 8fd5f7d792b3c968a561617eed41a3d1a4ed4e21
