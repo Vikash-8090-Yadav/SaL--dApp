3d8c6e818b6fa1a8b340370cc28034cf7e86bd89
Joseph2001-braganza <braganzajoseph2001@gmail.com>
2023-03-23 07:52:35 +0530

    Updated the Logout Button

--- Component/LogoutButton.js
+++ Component/LogoutButton.js
@@ -3,13 +3,28 @@ import Link from "next/link"
 
 
 const LogoutButton = ({ handleLogout }) => {
+    
     return (
+      <>
       <div>
-        <button className = "py-1.5 px-2 m-2 rounded-lg flex text-white bg-red-500 font-bold" onClick={handleLogout}>
+        <button className = "py-1.5 px-2 m-2 rounded-lg flex text-white bg-red-500 font-bold my-btn" 
+        onClick={handleLogout} 
+        >
             <div className = "text-md">LOGOUT</div> 
             <LogoutIcon size = {25} className = "ml-2" />
         </button>
         </div>
+
+        <style>
+        {`
+          @media (max-width: 767px) {
+            .my-btn {
+              display: none;
+            }
+          }
+        `}
+      </style>
+      </>
     )
 }
 export default LogoutButton
