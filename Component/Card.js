bbc946e95139a8246bb954144c8a8f3d26b07306
Bhavika Aggarwal <91602025+Bhavika-14@users.noreply.github.com>
2023-03-25 15:59:05 +0530

    Improved UI

--- Component/Card.js
+++ Component/Card.js
@@ -3,32 +3,42 @@ import { FaTwitter } from 'react-icons/fa';
 
 const Card = ({img}) => {
   return (
+
     <>
-      <div className = "w-100 bg-blue-500 shadow-xl">
+    <div className='bg-blue-500 p-4 rounded-lg'>
+      <div className = "w-100 bg-white shadow-xl rounded-lg">
         <div>
-          <img className = "h-30 w-full object-cover" src = {img} alt = "Profile"/>
+          <img className = "h-30 w-full object-cover rounded-t-lg" src = {img} alt = "Profile"/>
           <div className = "flex flex-col gap-4 p-4">
             <div>
-              <h2 className = "font-bold">Name</h2>
+              <h2 className = "font-bold text-center">Name</h2>
             </div>
 
             <div>
-              <p className = "text-justify">
+              <p className = "text-center">
                 The point of using Lorem Ipsum is that it has a normal distribution of English.
               </p>
             </div>
 
-            <div className = "flex flex-row justify-between">
-              <a className = "bg-white p-2 rounded-md text-blue-500" href = "#">Read More...</a>
-              <div className = "text-white/50 hover:text-white/80 py-2 font-semibold text-center">
-                <FaTwitter className = "text-xl cursor-pointer" size = {25} />
+            <div className = "flex flex-row justify-center content-center">
+              <div className="justify-center">
+                <a className = "bg-blue-500 p-2 rounded-lg text-white hover:bg-blue-800 text-center" href = "#">Read More...</a>
+              </div>
+              
+            </div>
+            <div className="flex flex-row justify-center object-center">
+              <div className = "text-blue-400 hover:text-blue-500 py-2 font-semibold text-center object-center items-center">
+                <FaTwitter className = "text-xl cursor-pointer text-center object-center" size = {25} />
               </div>
             </div>
           </div>
         </div>
       </div>
+      </div>
+
     </>
   )
 }
 
 export default Card
+
