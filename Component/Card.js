b792a75b80b67095ce57c5ef41b5fd6da5e9941f
tab21 <fatimatabassum2104@gmail.com>
2023-03-26 15:53:38 +0530

    added feedbacks and their images

--- Component/Card.js
+++ Component/Card.js
@@ -1,44 +1,60 @@
-import React from 'react'
-import { FaTwitter } from 'react-icons/fa';
+import React from "react";
+import Image from "next/image";
+import { FaTwitter } from "react-icons/fa";
 
-const Card = ({img}) => {
-  return (
+const Card = (data) => {
+	console.log("Client card data ------ ");
+	console.log(data.img);
 
-    <>
-    <div className='bg-blue-500 p-4 rounded-lg'>
-      <div className = "w-100 bg-white shadow-xl rounded-lg">
-        <div>
-          <img className = "h-30 w-full object-cover rounded-t-lg" src = {img} alt = "Profile"/>
-          <div className = "flex flex-col gap-4 p-4">
-            <div>
-              <h2 className = "font-bold text-center">Name</h2>
-            </div>
+	return (
+		<>
+			<div className="bg-blue-500 p-4 rounded-lg">
+				<div className="w-100 bg-white shadow-xl rounded-lg">
+					<div>
+						<Image
+							className="h-32 w-full object-cover rounded-t-lg"
+							src={data.img.src}
+							alt="Profile"
+							height={400}
+							width={500}
+						/>
+						<div className="flex flex-col gap-4 p-4">
+							<div>
+								<h2 className="font-bold text-center">
+									{data.name}
+								</h2>
+							</div>
 
-            <div>
-              <p className = "text-center">
-                The point of using Lorem Ipsum is that it has a normal distribution of English.
-              </p>
-            </div>
+							<div>
+								<p className="text-center line-clamp-5">
+									{data.feedback}
+								</p>
+							</div>
 
-            <div className = "flex flex-row justify-center content-center">
-              <div className="justify-center">
-                <a className = "bg-blue-500 p-2 rounded-lg text-white hover:bg-blue-800 text-center" href = "#">Read More...</a>
-              </div>
-              
-            </div>
-            <div className="flex flex-row justify-center object-center">
-              <div className = "text-blue-400 hover:text-blue-500 py-2 font-semibold text-center object-center items-center">
-                <FaTwitter className = "text-xl cursor-pointer text-center object-center" size = {25} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      </div>
+							<div className="flex flex-row justify-center content-center">
+								<div className="justify-center">
+									<a
+										className="bg-blue-500 p-2 rounded-lg text-white hover:bg-blue-800 text-center"
+										href="#"
+									>
+										Read More...
+									</a>
+								</div>
+							</div>
+							<div className="flex flex-row justify-center object-center">
+								<div className="text-blue-400 hover:text-blue-500 py-2 font-semibold text-center object-center items-center">
+									<FaTwitter
+										className="text-xl cursor-pointer text-center object-center"
+										size={25}
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
 
-    </>
-  )
-}
-
-export default Card
-
+export default Card;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./Component/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./Component/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
 
-  variants: {
-    extend: {
-      display: ['group-focus'],
-      opacity: ['group-focus'],
-      inset: ['group-focus']
-    },
-  },
-  
-  plugins: [],
-}
+	variants: {
+		extend: {
+			display: ["group-focus"],
+			opacity: ["group-focus"],
+			inset: ["group-focus"],
+		},
+	},
+
+	plugins: [require("@tailwindcss/line-clamp")],
+};
