869c54da881ccba02761d910365f55d7e671c637
Deepanshu <21bcs072@iiitdmj.ac.in>
2023-05-24 12:14:15 +0530

    Added scroll to top option

--- Component/Layout.js
+++ Component/Layout.js
@@ -4,7 +4,8 @@ import AboutApp from "./AboutApp";
 import Test from "./Test";
 import Contact from "./Contact";
 import Footer from "./Footer";
-import Mainc from "./mnc"
+import Mainc from "./mnc";
+import GoToTop from "./GoToTop";
 
 
 export default function Layout({children}) {
@@ -12,6 +13,7 @@ export default function Layout({children}) {
   console.log(children);
   return (
     <>
+      <GoToTop />
       <div >
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
