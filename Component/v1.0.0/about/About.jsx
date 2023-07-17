5fafecbe9e651f92735c9f4a2229e0a22d2486d7
Suruchi Kumari <suruchikumarimfp4@gmail.com>
2023-07-17 21:37:01 +0530

    Update About.jsx

--- Component/v1.0.0/about/About.jsx
+++ Component/v1.0.0/about/About.jsx
@@ -6,11 +6,6 @@ import styled from "styled-components";
 const MyDiv = styled.div`
   width: 686px;
   height: 234px;
-
-  @media (max-width: 1000px) {
-    width: 100%;
-    height: auto;
-  }
 `;
 
 const About = () => {
@@ -20,11 +15,11 @@ const About = () => {
         <div className="text-center font-lily-script-one bg-gradient-to-b from-[#FB65FE] from-100% to-[#9010FF] to-100% text-transparent bg-clip-text p-5 text-4xl">
           About Us
         </div>
-        <div className="flex justify-center pl-20 pr-20">
-          <div className="md:mt-[-45px]">
+        <div className="flex flex-col lg:flex-row justify-center pl-20 pr-20">
+          <div className="lg:mt-[-45px] flex justify-center">
             <Image src={AboutIcon} alt="about-icon"></Image>
           </div>
-          <MyDiv className="font-Inter text-xl text-[#000000] font-normal leading-10 md:pt-20 pt-5">
+          <MyDiv className="font-Inter text-xl text-[#000000] font-normal leading-10 lg:pt-20 pt-5">
             The Sal-dApp provides an efficient platform for organizational
             administrators to effectively oversee and manage their employees'
             salaries by adding them as users and securely storing their
